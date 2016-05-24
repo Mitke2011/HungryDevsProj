f846f8b58b57f6b4ceb0f2b0060640407ba823d4
Stefan Mitić <mitkeprepecenica@gmail.com>
2016-05-25 01:36:34 +0200

    Partial View
    Added a _Form partial View to User Edit View.

--- HungryDevs/Controllers/UsersController.cs
+++ HungryDevs/Controllers/UsersController.cs
@@ -104,7 +104,9 @@ namespace HungryDevs.Controllers
         {
             User u = Session["currentUser"] as User;
             if (u != null)
-            {
+            {                
+                TempData["TransUser"] = UsersManager.Current.GetUser(id);
+                object p = TempData[""];
                 ViewBag.HelloUser = string.Format("{0}-{1}", u.FirstName, u.Id);
                 return View(UsersManager.Current.GetUser(id));
             }
